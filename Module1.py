2187cca2c8352fae11ec3e80eef06cba8c1dc3bb
deepanshu <deepanshuchg@gmail.com>
2020-04-20 21:37:11 +0530

    Added more functionalities - Now the bot opens each subreddit and get top 5 posts for each subreddit.

--- Module1.py
+++ Module1.py
@@ -3,29 +3,48 @@ from selenium import webdriver
 import time
 
 #driver = webdriver.Chrome()
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--disable-notifications")                      #This is to prevent the "Allow notification" dialog box which interferes with the code
-driver = webdriver.Chrome(chrome_options=chrome_options)
+#chrome_options = webdriver.ChromeOptions()
+#chrome_options.add_argument("--disable-notifications")                      #This is to prevent the "Allow notification" dialog box which interferes with the code
+#driver = webdriver.Chrome(chrome_options=chrome_options)
 
-driver.get("https://www.reddit.com/login/")                                 #Opening the login site directly
-driver.maximize_window()                                
-#time.sleep(10)
+def login_subreddit(driver):
+    driver.get("https://www.reddit.com/login/")                                 #Opening the login site directly
+    #driver.maximize_window()                                
+    #time.sleep(10)
 
-f=open("loginInformation.py","r")                                           #Reading username and passwords                    
-lines=f.readlines()
-username=lines[4].split('"')[1]                                                 
-password=lines[5].split('"')[1]
-f.close()
+    f=open("loginInformation.py","r")                                           #Reading username and passwords                    
+    lines=f.readlines()
+    username=lines[4].split('"')[1]                                                 
+    password=lines[5].split('"')[1]
+    f.close()
 
-time.sleep(2)                                                               #Giving time to load the page
+    time.sleep(2)                                                               #Giving time to load the page
 
 
-driver.find_element_by_xpath('/html/body/div/div/div[2]/div/form/div/fieldset[1]/input').send_keys(username)        #Entering username
+    driver.find_element_by_xpath('/html/body/div/div/div[2]/div/form/div/fieldset[1]/input')\
+    .send_keys(username)        #Entering username
 
-driver.find_element_by_xpath('//*[@id="loginPassword"]').send_keys(password)                                        #Entering password
+    driver.find_element_by_xpath('//*[@id="loginPassword"]')\
+    .send_keys(password)                                        #Entering password
 
-driver.find_element_by_xpath('//button[@class="AnimatedForm__submitButton"]').click()                               #Clicking the logging button
+    driver.find_element_by_xpath('//button[@class="AnimatedForm__submitButton"]').click()                               #Clicking the logging button
 
-time.sleep(10)                                                                                                      #Giving time to user to see before the brower closes
+    time.sleep(4)
 
-driver.close()
+    driver.find_element_by_xpath('//*[@id="SHORTCUT_FOCUSABLE_DIV"]/div[1]/header/div/div[1]/div[2]/button').click()
+
+    time.sleep(5)
+
+    #subreddits = driver.find_elements_by_xpath('class="XEkFoehJNxIH9Wlr5Ilzd _2MgAHlPDdKvXiG-Qbz5cbC "')                                                                                                      
+    subreddits = driver.find_elements_by_xpath('//*[@class="_2aqH0n-kSzFY7HZZ5GL-Jb"]')                                                                                                      
+
+    leng = len(subreddits)
+    #for i in range(5,leng):
+    #    print(subreddits[i].text)
+    #    subreddit_list= subreddits[i].text
+    subreddit_names = []
+    for i,subreddit in enumerate(subreddits):
+        if i>=5:
+            subreddit_names.append(subreddit.text)
+    
+    return subreddit_names
--- Module3.py
+++ Module3.py
@@ -1,1 +1,27 @@
 # upvote post
+from selenium import webdriver
+from time import sleep
+
+def find_top_posts(driver):
+    print("In find_top function")
+    posts = driver.find_elements_by_xpath('//h3[@class="_eYtD2XCVieq6emjKBH3m"]')
+    print(posts)
+    for post in posts:
+        print(post.text)
+
+def go_to_subreddit(driver, subreddit_name):
+    xp = f"//span[text()='{subreddit_name}']"
+    driver.find_element_by_xpath(xp).click()
+    sleep(5)
+    posts = driver.find_elements_by_xpath('//h3[@class="_eYtD2XCVieq6emjKBH3m"]')
+    print(f"\nPosts in {subreddit_name} are: " )
+    post_list = []
+    for i in range(5):
+        print(f"{i+1}. {posts[i].text}")
+        post_list.append((posts[i]))
+
+    #Calling openpyxl function function(post_list)
+
+    sleep(5)
+    driver.find_element_by_xpath('//*[@id="SHORTCUT_FOCUSABLE_DIV"]/div[1]/header/div/div[1]/div[2]/button').click()
+
