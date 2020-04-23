6d3ccaa66a0b504153a2f8d95c9a1323d311bdc0
Cerve <59832296+cerver1@users.noreply.github.com>
2020-04-22 21:18:44 -0400

    module 1 code and comment clean up

--- Module1.py
+++ Module1.py
@@ -1,57 +1,52 @@
-#Log in and returns subreddit names
 from selenium import webdriver
 from time import sleep
 import loginInformation
 
-#driver = webdriver.Chrome()
-#chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_argument("--disable-notifications")                      #This is to prevent the "Allow notification" dialog box which interferes with the code
-#driver = webdriver.Chrome(chrome_options=chrome_options)
+"""
+
+Log into Reddit the account && Return all subreddits 
+
+"""
 
 def login_subreddit(driver):
+    
     driver.get("https://www.reddit.com/login/")                                 #Opening the login site directly
-    #driver.maximize_window()                                
-    #time.sleep(10)
 
     username = loginInformation.login_username
     password = loginInformation.login_password
 
-    sleep(2)                                                               #Giving time to load the page
+    sleep(2)                                                               
 
 
-    driver.find_element_by_xpath('/html/body/div/div/div[2]/div/form/div/fieldset[1]/input')\
-    .send_keys(username)        #Entering username
+    username_input = driver.find_element_by_xpath('//*[@id="loginUsername"]')
+    username_input.send_keys(username)
 
-    driver.find_element_by_xpath('//*[@id="loginPassword"]')\
-    .send_keys(password)                                        #Entering password
+    password_input = driver.find_element_by_xpath('//*[@id="loginPassword"]')
+    password_input.send_keys(password)                              
 
-    driver.find_element_by_xpath('//button[@class="AnimatedForm__submitButton"]').click()                               #Clicking the logging button
+    sleep(1)
 
-    sleep(4)
+    login_confirm = driver.find_element_by_xpath('//*[@type="submit"]')
+    login_confirm.click()
 
+    sleep(2)
 
-
-    #subreddits = driver.find_elements_by_xpath('class="XEkFoehJNxIH9Wlr5Ilzd _2MgAHlPDdKvXiG-Qbz5cbC "')   
     
 def subreddit_name_provider(driver):
 
     sleep(2)
 
-    open_navigation = driver.find_element_by_xpath('//*[@id="SHORTCUT_FOCUSABLE_DIV"]/div[1]/header/div/div[1]/div[2]/button').click()
-    open_navigation
+    subreddit_navigation = driver.find_element_by_xpath('//button[@tabindex=0]')
+    subreddit_navigation.click()
 
-    sleep(5)
+    sleep(4)
 
-    subreddits = driver.find_elements_by_xpath('//*[@class="_2aqH0n-kSzFY7HZZ5GL-Jb"]')                                                                                                      
-
-    # leng = len(subreddits)
-    # for i in range(5,leng):
-    #    print(subreddits[i].text)
-    #    subreddit_list= subreddits[i].text
+    subreddit_list = driver.find_elements_by_xpath('//*[@class="_2aqH0n-kSzFY7HZZ5GL-Jb"]')                                                                                                     
 
     subreddit_names = []
-    for i,subreddit in enumerate(subreddits):
-        if i>=5:                                                    #Skipping the subreddit names in favorite section
+    
+    for i,subreddit in enumerate(subreddit_list):
+        if i>=5:
             subreddit_names.append(subreddit.text)
     
     return subreddit_names
--- RedditBot.py
+++ RedditBot.py
@@ -6,7 +6,7 @@ from Module2 import browser_close
 from Module3 import go_to_subreddit
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--disable-notifications")                      #This is to prevent the "Allow notification" dialog box which interferes with the code
+chrome_options.add_argument("--disable-notifications")                      # This is to prevent the "Allow notification" dialog box which interferes with the code
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
 
