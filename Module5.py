59bed5e79b85d9117dc7378756e3c1100f6ac5a2
Cerve <59832296+cerver1@users.noreply.github.com>
2020-04-25 12:48:22 -0400

    Module 5 && Module 4 clean up

--- Module5.py
+++ Module5.py
@@ -5,30 +5,35 @@ from selenium import webdriver
 wb = openpyxl.load_workbook('reddit_posts.xlsx')               #This connected with the excel file
 sheet = wb.active
 
-def check_score(post_dict, driver):
+def check_score(post_dict,driver):
 
-    for keys in post_dict:
-        if(post_dict[keys][-1] == "k"):                                                 #This part of program is to remove k from the score variable like '18.2k' 
-            post_dict[keys] = float(post_dict[keys][:-1])
-            post_dict[keys] = (post_dict[keys] * 1000)                                  #This will change the score to normal integer
-        try:
-            if(int(post_dict[keys]) > 300):
-                print(f"This post has more than 300 votes: {keys}")
-                add_data_excel(keys, driver)                                            #Calling function which will verify if the entry is present in excel, if not it will add it
-        except ValueError:
-            pass
+    add_data_excel(post_dict,driver)
 
-def add_data_excel(post, driver):
-    search_row = 0                                       # Will store the row where the entry is found, will be used as a flag variable                                              
-    for i in range(1,sheet.max_row + 1):
-        if(sheet.cell(row = i,column = 1).value == post):   # Searching if the entry is present in the data or not
-            search_row = i
-        
-    if(search_row == 0):                                                    # If the element is not present in the excel file
-        #adding data to excel
-        post_upvote(driver)
-        sheet.append([post])                                              # Adding 0 just because append method takes a list of more than 1 value
-        wb.save('reddit_posts.xlsx')                                      # Saving the file
-    else:
-        print()
-        # We can call the upvote function from here since the entry is not present in excel file and the score is greater than 300 or any number
+#    for keys in post_dict:
+#        if(post_dict[keys][-1] == "k"):                                                 #This part of program is to remove k from the score variable like '18.2k' 
+#            post_dict[keys] = float(post_dict[keys][:-1])
+#            post_dict[keys] = (post_dict[keys] * 1000)                                  #This will change the score to normal integer
+#        try:
+#            add_data_excel(keys, driver)                                            #Calling function which will verify if the entry is present in excel, if not it will add it
+#        except ValueError:
+#            pass
+
+"""
+fix the dictionary result in Module 3
+"""
+def add_data_excel(post_dict,driver):
+    
+    list_of_saved_links = []
+
+    for i in sheet['A']:
+        list_of_saved_links.append(i.value)
+    
+    for key in post_dict:
+        if key in list_of_saved_links:
+            print("This post already exists")
+        else:
+            sheet.append([key])
+            post_upvote(driver)
+            wb.save('reddit_posts.xlsx') 
+            print("This is a new post!")
+    
