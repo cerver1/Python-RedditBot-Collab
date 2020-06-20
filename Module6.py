af23b8c9e8b3e548e8828d5b4cad097beb06e47b
Cerve <59832296+cerver1@users.noreply.github.com>
2020-06-20 13:26:48 -0400

    streamline browser automation opening

--- Module6.py
+++ Module6.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-print(webdriver.DesiredCapabilities['browserVersion'])
+def browserConfig():
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--disable-notifications")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+    return driver
--- RedditBot.py
+++ RedditBot.py
@@ -1,12 +1,13 @@
 # BotBrain
 
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from Module1 import login_subreddit , subreddit_name_provider
 from Module2 import browser_close
 from Module3 import go_to_subreddit
+from Module6 import browserConfig as mainDriver
 
-
-driver = webdriver.Chrome('./ChromeDriver/driver83.exe')
+driver = mainDriver()
 
 login_subreddit(driver)
 
