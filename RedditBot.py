fe61767a7825e493de6b21a9b2566df9f6d5ec0b
Cerve <59832296+cerver1@users.noreply.github.com>
2020-06-19 22:06:57 -0400

    chomedriver upkeep

--- RedditBot.py
+++ RedditBot.py
@@ -5,9 +5,8 @@ from Module1 import login_subreddit , subreddit_name_provider
 from Module2 import browser_close
 from Module3 import go_to_subreddit
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--disable-notifications")                      # This is to prevent the "Allow notification" dialog box which interferes with the code
-driver = webdriver.Chrome(chrome_options=chrome_options)
+
+driver = webdriver.Chrome('./ChromeDriver/driver83.exe')
 
 login_subreddit(driver)
 
