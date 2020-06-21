aa544d509ac23df2b3907846e82f520fb43ecc6a
Cerve <59832296+cerver1@users.noreply.github.com>
2020-06-21 15:47:04 -0400

    6/21/20

--- RedditBot.py
+++ RedditBot.py
@@ -7,13 +7,18 @@ from Module2 import browser_close
 from Module3 import go_to_subreddit
 from Module6 import browserConfig as mainDriver
 
+# Open browser
 driver = mainDriver()
 
+# Logs you into account 
 login_subreddit(driver)
 
+# Gets the names of each subreddit
 subreddit_names = subreddit_name_provider(driver)
 
+# Go to your subreddits
 for i in range((len(subreddit_names))):
   go_to_subreddit(driver, subreddit_names[i])
 
+# Closes down the browser
 browser_close(driver)
